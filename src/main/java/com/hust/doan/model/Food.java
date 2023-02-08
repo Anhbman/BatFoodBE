473d5a592eacfd8c99cadb37b6a69a7c0614338e
bumman <anhbumbk@gmail.com>
2023-02-08 17:13:14 +0700

    test

--- src/main/java/com/hust/doan/model/Food.java
+++ src/main/java/com/hust/doan/model/Food.java
@@ -1,5 +1,6 @@
 package com.hust.doan.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 import javax.persistence.*;
@@ -14,6 +15,7 @@ public class Food extends BaseEntity{
     private Float price;
 
     @OneToMany(mappedBy = "food")
+    @JsonIgnore
     private Set<OrderFood> orderFoods;
 
     @ManyToOne
